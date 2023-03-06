164e1edc20e99b34ead778a2dc7390c688b3d114
kevinjhluo <97573036+kevinjhluo@users.noreply.github.com>
2023-03-06 13:00:16 -0800

    Update ee250_lab6.py

--- ee250_lab6.py
+++ ee250_lab6.py
@@ -1,4 +1,5 @@
 import grovepi
+import time
 from grove_rgb_lcd import *
 
 potentiometer = 0
@@ -7,8 +8,8 @@ ultrasonic_ranger = 6
 while True:
    sensor_value = grovepi.analogRead(potentiometer)
    ultrasonic_value = grovepi.ultrasonicRead(ultrasonic_ranger)
-
-if(sensor_value > ultrasonic_value):
-      setText(f"{sensor_value}cm OBJ PRES \n {ultrasonic_value}cm")
-else:
-      setText(f"{sensor_value}cm \n {ultrasonic_value}cm")
+   if(sensor_value > ultrasonic_value):
+      setText_norefresh(f"{sensor_value}cm OBJ PRES \n {ultrasonic_value}cm")
+   else:
+      setText_norefresh(f"{sensor_value}cm \n {ultrasonic_value}cm")
+   time.sleep(0.2)
